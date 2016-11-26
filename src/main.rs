220e13136fe008866a1cd5343fab47d6b68eb8f5
johnervine@hotmail.com <johnervine@hotmail.com>
2016-11-26 17:12:31 +0000

    Updated find_with_ext to take an actual extension

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     if let Some(arg) = env::args().nth(1) {
         let arg_path = PathBuf::from(arg);
         if arg_path.is_dir() {
-            find_with_ext(&arg_path, &mut vm_file_paths)
+            find_with_ext(&arg_path, "vm", &mut vm_file_paths)
         } else {
             vm_file_paths.push(arg_path)
         }
@@ -18,16 +18,15 @@ fn main() {
     println!("Translating the following files into {:?}, Hack machine Language", vm_file_paths)
 }
 
-fn find_with_ext(dir: &Path, vec: &mut Vec<PathBuf>) {
+fn find_with_ext(dir: &Path, ext: &str, vec: &mut Vec<PathBuf>) {
     if let Ok(entries) = fs::read_dir(dir) {
         //Retrieve File metadata
         for entry in entries {
             if let Ok(entry) = entry {
                 if let Some(e) = entry.path().extension() {
                     if let Some(exstr) = e.to_str() {
-                        match exstr {
-                            "vm" => vec.push(entry.path()),
-                            _ => {}
+                        if ext == exstr {
+                            vec.push(entry.path())
                         }
                     }
                 }
