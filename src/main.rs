32eaa133d9ffe36b98521e79b3227b9c52f9b86f
johnervine@hotmail.com <johnervine@hotmail.com>
2016-11-28 06:42:13 +0000

    Updated file gathering functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::path::{Path, PathBuf};
 use std::fs;
+use std::io;
+use std::ffi::OsStr;
 
 fn main() {
     //Files with .vm extension
@@ -8,7 +10,7 @@ fn main() {
     if let Some(arg) = env::args().nth(1) {
         let arg_path = PathBuf::from(arg);
         if arg_path.is_dir() {
-            find_with_ext(&arg_path, "vm", &mut vm_file_paths)
+            push_entries_with_ext(&arg_path, "vm", &mut vm_file_paths).unwrap();
         } else {
             vm_file_paths.push(arg_path)
         }
@@ -18,19 +20,33 @@ fn main() {
     println!("Translating the following files into {:?}, Hack machine Language", vm_file_paths)
 }
 
-fn find_with_ext(dir: &Path, ext: &str, vec: &mut Vec<PathBuf>) {
-    if let Ok(entries) = fs::read_dir(dir) {
-        //Retrieve File metadata
-        for entry in entries {
-            if let Ok(entry) = entry {
-                if let Some(e) = entry.path().extension() {
-                    if let Some(exstr) = e.to_str() {
-                        if ext == exstr {
-                            vec.push(entry.path())
-                        }
-                    }
-                }
+pub fn push_entries_with_ext(dir: &Path,
+                             ext: &str,
+                             buf: &mut Vec<PathBuf>)
+                             -> io::Result<()>
+{
+    let dir_iter = try!(fs::read_dir(dir));
+    for maybe_entry in dir_iter {
+        let entry = try!(maybe_entry);
+        if let Some(e) = ext_from_path(&entry.path()) {
+            if ext_match(ext, &e) {
+                buf.push(entry.path());
             }
         }
+    }
+    Ok(())
+}
+
+fn ext_from_path<'a>(path: &'a Path) -> Option<&'a OsStr> {
+    match path.extension() {
+        Some(ext) => Some(ext),
+        None => None
+    }
+}
+
+fn ext_match(ext: &str, other: &OsStr) -> bool {
+    match other.to_str() {
+        Some(s) => ext == s,
+        None => false
     }
 }
