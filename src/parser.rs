ee3a537aaca4559449a706c1e05ce16f01839f24
Ervine <john.ervine@cmegroup.com>
2016-11-29 19:03:20 +0000

    Fixed bug with arg1 not being the Arithmetic operator when required

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufReader};
 
+#[derive(PartialEq)]
 #[derive(Debug)]
 pub enum CommandType {
     C_ARITHMETIC,
@@ -20,7 +21,7 @@ pub enum CommandType {
 #[derive(Debug)]
 pub struct VMCommand {
     command_type: CommandType,
-    arg1:         Option<String>,
+    arg1:         String,
     arg2:         Option<String>
 }
 
@@ -51,21 +52,24 @@ fn is_blank(line: &str) -> bool {
 fn new_vm_command(line: &str) -> VMCommand {
     let mut spli = line.split_whitespace();
 
-    let command = match spli.next() {
-        Some("push") => CommandType::C_PUSH,
-        Some("pop")  => CommandType::C_POP,
-        _            => CommandType::C_ARITHMETIC
+    let command = spli.next().unwrap();
+    let command_type = match command {
+        "push" => CommandType::C_PUSH,
+        "pop"  => CommandType::C_POP,
+        _      => CommandType::C_ARITHMETIC
     };
-    
-    let arg1 = match spli.next() {
-        Some(a) => Some(a.to_string()),
-        None    => None
-    };
+
+    let mut arg1;
+    if command_type == CommandType::C_ARITHMETIC {
+        arg1 = command.to_string();
+    } else {
+        arg1 = spli.next().unwrap().to_string();
+    }
 
     let arg2 = match spli.next() {
         Some(a) => Some(a.to_string()),
         None    => None
     };
 
-    return VMCommand{command_type: command, arg1: arg1, arg2: arg2};
+    return VMCommand{command_type: command_type, arg1: arg1, arg2: arg2};
 }
