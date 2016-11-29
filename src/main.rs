307db7671833281f0cb0089c166fe30da77157e4
Ervine <john.ervine@cmegroup.com>
2016-11-29 08:38:33 +0000

    added recursive function to walk directory

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::fs;
 use std::io;
 use std::ffi::OsStr;
 
+fn push_path_to(p: PathBuf, v: &mut Vec<PathBuf>) {
+    v.push(p);
+}
+
 fn main() {
     //Files with .vm extension
     let mut vm_file_paths: Vec<PathBuf> = vec![];
@@ -20,10 +24,7 @@ fn main() {
     println!("Translating the following files into {:?}, Hack machine Language", vm_file_paths)
 }
 
-pub fn push_entries_with_ext(dir: &Path,
-                             ext: &str,
-                             buf: &mut Vec<PathBuf>)
-                             -> io::Result<()>
+pub fn push_entries_with_ext(dir: &Path, ext: &str, buf: &mut Vec<PathBuf>) -> io::Result<()>
 {
     let dir_iter = try!(fs::read_dir(dir));
     for maybe_entry in dir_iter {
@@ -49,4 +50,19 @@ fn ext_match(ext: &str, other: &OsStr) -> bool {
         Some(s) => ext == s,
         None => false
     }
+}
+
+fn visit_dirs(dir: &Path, ext: &str,  buf: &mut Vec<PathBuf>, cb: &Fn(PathBuf, &mut Vec<PathBuf>)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in try!(fs::read_dir(dir)) {
+            let entry = try!(entry);
+            let path = entry.path();
+            if path.is_dir() {
+                try!(visit_dirs(&path, ext, buf, cb));
+            } else {
+                cb(path, buf);
+            }
+        }
+    }
+    Ok(())
 }
